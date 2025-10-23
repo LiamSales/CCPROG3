1dd966948dd51c45b87ca5814dc405a152995e84
LiamSales <liam_miguel_sales@dlsu.edu.ph>
2025-10-23 21:33:26 +0800

    new stuff on the vending machine classes

---
+++ SpecialMachine.kt
@@ -0,0 +1,3 @@
+class SpecialMachine(change: Int) : VendingMachine(change){
+    
+}
--- VendingMachine.kt
+++ VendingMachine.kt
@@ -1,5 +1,27 @@
-class VendingMachine (var change: Float){
-    val slots: Array<Item>(8) = emptyArray()
+class VendingMachine (var slotLimit: Int, var itemLimit: Int){
+
+    init{
+        require(slotLimit >= 10, "It must have at least 10 slots.")
+        require(itemLimit >= 8, "It must have at least 8 items per slot.")        
+    }
+
+    var change: Float = 0.0f
+    val slots: Array<Item>(slotLimit) = emptyArray()
+
+
+    fun collectChange(){
+        this.change = 0.0f
+    }
+
+    fun payMachine(cash: Float){
+        change+=cash
+    }
+
+    fun displayBalance(){
+        println("change")
+    }
 
     
+
+
 }
