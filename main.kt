9e212b575dab5f197df21d9408f8c75da1a80854
LiamSales <liam_miguel_sales@dlsu.edu.ph>
2025-10-30 22:24:33 +0800

    no need to validate

--- main.kt
+++ main.kt
@@ -1,36 +1,12 @@
 typealias cash = MutableMap<Int,Int>
-// can i hardcode the denominations here globally?
 
 data class Item(
     val name: String,
     var calories: Int
 )
 
-//the whole thing isnt hardcoded, items are saved in a txt file
-//do i save the machines as well?
+//no need to worry about input validation since GUI will handle all of that
 
-fun validateInput(input: String?, max: Int): String{
-    var current = input
-    do {
-        if (current != null && current.length <= max && current.isNotEmpty())
-            return current
-        println("Input can only be up to $max characters long. Please try again:")
-        current = readlnOrNull()
-    } while(true)
-}
-//have chatgpt proofread whether null check is actually neessary, workaround needed and if i can just do readln instead of readlnOrNull such that user really cant place null
-
-
-fun validateInput(input: Int): Int{
-    return 0
-}
-
-fun validateInput(input: Float): Float{
-    return 0f
-}
-
-
-//can this have input validation? limited characters for string; non negative calories
 
 fun createRegular(){
 
@@ -69,7 +45,7 @@ fun testMachine(){
 }
 
 fun testFeatures(){
-    //automatic check
+    //automatic check whether if special or regular
 }
 
 fun testMaintenance(){
