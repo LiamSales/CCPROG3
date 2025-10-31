7f8cec302fa93124e73609d689bb863834917060
LiamSales <liam_miguel_sales@dlsu.edu.ph>
2025-10-31 23:12:43 +0800

    Maintenance

--- VendingMachine.kt
+++ VendingMachine.kt
@@ -1,3 +1,4 @@
+
 data class Item(
     val name: String,
     var calories: Int
@@ -24,6 +25,29 @@ class VendingMachine (val slotLimit: Int, val itemLimit: Int){
     val slots = Array(slotLimit) { Slot(null, 0, 0f) }
     val register = CashRegister()
     val transactions = ArrayList<Transaction>()
+
+    //should only be available to the user if there is an available slot
+    fun setSlot(item: Item, price: Float) {
+
+        val i = this.slots.indexOfFirst { it.item == null }
+
+        slots[i].item = item
+        slots[i].price = price
+        println("Added ${item.name} to slot #${i + 1}.")
+
+    }
+
+    fun clearSlot(i: Int){
+
+        this.slots[i].item = null
+        this.slots[i].quantity = 0
+        this.slots[i].price = 0f
+    
+    }
+
+    fun restockQuantity(i: Int, quantity: Int){
+        this.slots[i].quantity = quantity
+    }
 
 
 }
