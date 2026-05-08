675668fa4b2153bc87ab9a8dd801ab0c8098a924
Liam Miguel V. Sales <liam_miguel_sales@dlsu.edu.ph>
2026-05-08 23:08:21 +0800

    retry

--- test/build.grade.kts
+++ test/build.grade.kts
@@ -1,15 +1,20 @@
 plugins {
     kotlin("jvm") version "1.9.0"
     application
+    id("org.openjfx.javafxplugin") version "0.0.13"
 }
 
 repositories {
     mavenCentral()
 }
 
+javafx {
+    version = "17"
+    modules = listOf("javafx.controls", "javafx.fxml")
+}
+
 dependencies {
-    implementation("org.openjfx:javafx-controls:21")
-    implementation("org.openjfx:javafx-fxml:21")
+    implementation(kotlin("stdlib"))
 }
 
 application {
