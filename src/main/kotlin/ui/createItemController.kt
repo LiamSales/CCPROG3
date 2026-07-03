a94b7aa0071d906ec71abc1a0dd54e35b7411e65
Liam Miguel V. Sales <liam_miguel_sales@dlsu.edu.ph>
2026-07-03 22:18:08 +0800

    spinner doesndt work for some reason (im sure i isolated the problem to that)

--- src/main/kotlin/ui/createItemController.kt
+++ src/main/kotlin/ui/createItemController.kt
@@ -1,5 +1,76 @@
 package ui
 
+import javafx.event.ActionEvent
+import javafx.fxml.FXML
+import javafx.fxml.FXMLLoader
+import javafx.scene.Parent
+import javafx.scene.Scene
+import javafx.scene.control.TextField
+import javafx.scene.control.Spinner
+import javafx.scene.control.SpinnerValueFactory
+import javafx.stage.FileChooser
+import javafx.stage.Stage
+import java.io.File
+
 class CreateItemController {
+
+    @FXML
+    private lateinit var nameField: TextField
+
+
+    @FXML
+    private lateinit var calorieSpinner: Spinner<Int>
+
+    @FXML
+    private lateinit var imagePathField: TextField
+
+    @FXML
+    fun browseImage() {
+
+        val chooser = FileChooser()
+
+        chooser.title = "Choose Item Image"
+
+        chooser.extensionFilters.add(
+
+            FileChooser.ExtensionFilter(
+                "Images",
+                "*.png",
+                "*.jpg",
+                "*.jpeg"
+            )
+
+        )
+
+        val stage =
+            nameField.scene.window as Stage
+
+        val file: File? =
+            chooser.showOpenDialog(stage)
+
+        if (file != null) {
+
+            imagePathField.text = file.absolutePath
+
+        }
+    }
+
+    @FXML
+    fun submitItem(event: ActionEvent) {
+
+        // Saving will be added later.
+
+        val root: Parent =
+            FXMLLoader.load(
+                javaClass.getResource("/fxml/main.fxml")
+            )
+
+        val stage =
+            (event.source as javafx.scene.Node)
+                .scene
+                .window as Stage
+
+        stage.scene = Scene(root)
+    }
 
 }
